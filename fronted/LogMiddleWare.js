ab0a9fbb3d51f9498453c67cabffcbbdcc250d92
useru1k <uvvi571@gmail.com>
2025-09-04 15:17:30 +0530

    change the log module fix syntax error

---
+++ fronted/LogMiddleWare.js
@@ -0,0 +1,21 @@
+function Log(stack, level, pkg, message) {
+  const logData = {
+    stack,
+    level,
+    package,
+    message,
+  };
+
+
+try {
+    fetch("http://20.244.56.144/evaluation-service//logs", {  
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(logData),
+    );
+  } catch (error) {
+    console.error("Wornfg:", error);
+  }
+}
+
+module.export = { LogMiddleWare };
